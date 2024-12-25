093f072104ff038f1c7b399420020b2613a35f02
GitHub Action <action@github.com>
2024-12-25 02:50:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/liubin95/log2csv/MyBundle.kt
+++ src/main/kotlin/com/github/liubin95/log2csv/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.liubin95.log2csv
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/liubin95/log2csv/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liubin95/log2csv/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.liubin95.log2csv.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liubin95.log2csv.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
